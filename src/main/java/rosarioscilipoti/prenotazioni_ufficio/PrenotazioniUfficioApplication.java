d6f23f7cd8e6b777a51f5e9339044085987c75c3
Rosario Scilipoti <rosariosca12@icloud.com>
2024-02-09 13:27:08 +0100

    Initial commit

---
+++ src/main/java/rosarioscilipoti/prenotazioni_ufficio/PrenotazioniUfficioApplication.java
@@ -0,0 +1,13 @@
+package rosarioscilipoti.prenotazioni_ufficio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrenotazioniUfficioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrenotazioniUfficioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rosarioscilipoti/prenotazioni_ufficio/PrenotazioniUfficioApplicationTests.java
@@ -0,0 +1,13 @@
+package rosarioscilipoti.prenotazioni_ufficio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrenotazioniUfficioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
